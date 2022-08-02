f63a2fe4e1ccf598917f83a29e3250f8598e9276
Ian Harvey <108738675+Harvey1968@users.noreply.github.com>
2022-08-02 21:48:34 +0100

    Update PythonLab4_3_1_6.py

--- PythonLab4-3-1-6/PythonLab4-3-1-6/PythonLab4_3_1_6.py
+++ PythonLab4-3-1-6/PythonLab4-3-1-6/PythonLab4_3_1_6.py
@@ -16,8 +16,8 @@ def is_year_leap(year):
 		return True
 
 # "Short testing code"
-test_data = [1900, 2000, 2016, 1987, 1985]		# The 5th element added, 1985 should be False, but has 
-test_results = [False, True, True, False, True]	# been incorrectly entered to examine the testing code.
+test_data = [1900, 2000, 2016, 1987, 1985]	# The 5th element added, 1985 should be False, but has 
+test_results = [False, True, True, False, True] # been incorrectly entered to examine the testing code.
 for i in range(len(test_data)):
 	year = test_data[i]
 	print(year,"-> ",end="")
